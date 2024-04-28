ca0441fadab5c0bdfce9221e10a34b7fe63fd9fd
tim_curchod <timofeyc@hotmail.com>
2024-04-28 19:30:56 +1000

    resolve the (blocked:mixed-content) issue by setting axios default to https

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://3.26.241.7:8000/";
+axios.defaults.baseURL = "https://3.26.241.7:8000/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
